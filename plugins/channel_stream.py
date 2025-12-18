df5332bbf35c5532932985e592eb70d0004873db
Mdmuzaffarimam <muzaffarimam1@yahoo.com>
2025-12-18 17:49:33 +0530

    Update channel_stream.py
    Signed-off-by: Mdmuzaffarimam <muzaffarimam1@yahoo.com>

--- plugins/channel_stream.py
+++ plugins/channel_stream.py
@@ -46,7 +46,7 @@ async def channel_receive_handler(bot: Client, broadcast: Message):
             text=f"**Channel Name:** `{broadcast.chat.title}`\n**CHANNEL ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream}",
             quote=True
         )
-        new_caption = CHANNEL_FILE_CAPTION.format(CHANNEL, file_name)
+        new_caption = CHANNEL_FILE_CAPTION.format(CHANNEL, file_caption}
         buttons_list = [
             [InlineKeyboardButton("• ꜱᴛʀᴇᴀᴍ •", url=stream),
              InlineKeyboardButton("• ᴅᴏᴡɴʟᴏᴀᴅ •", url=download)],
